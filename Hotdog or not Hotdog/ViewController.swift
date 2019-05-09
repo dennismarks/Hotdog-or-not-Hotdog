2bdb7a6b5bd24d7adddd850ab904ef1779a9beb8
dennismarks <dennismarkshome@gmail.com>
2019-05-09 18:25:36 -0400

    implement UI image picker controller

---
+++ Hotdog or not Hotdog/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Hotdog or not Hotdog
+//
+//  Created by Dennis M on 2019-05-09.
+//  Copyright © 2019 Dennis M. All rights reserved.
+//
+
+import UIKit
+import CoreML
+import Vision
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    let imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+    }
+    
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+}
+
+extension ViewController: UIImagePickerControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = userPickedImage
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+}
+
+
+extension ViewController: UINavigationControllerDelegate {
+    
+}
